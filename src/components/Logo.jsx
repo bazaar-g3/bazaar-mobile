ecb9697588e7b0c5f4df66f80ab4022b0a985957
Martina Gualdi_2 <martugualdi@gmail.com>
2026-04-05 18:10:26 -0300

    Pongo estetica de perfil acorde a las otras paginas, hago el tamaño del logo editable para poder ponerlo acorde a lo necesitado en cada pagina

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,23 +2,31 @@ import { View, Text, StyleSheet } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { COLORS } from '../constants/colors'
 
-export default function Logo({ size = 34, textSize = 30 }) {
+export default function Logo({
+  size = 34,
+  textSize = 30,
+  spacing = 8,
+  showText = true,
+  style,
+}) {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, style]}>
       <MaterialCommunityIcons
         name="storefront-outline"
         size={size}
         color={COLORS.primaryLight}
       />
 
-      <Text style={[styles.text, { fontSize: textSize }]}>
-        <Text style={{ color: COLORS.logoB }}>B</Text>
-        <Text style={{ color: COLORS.logoA }}>A</Text>
-        <Text style={{ color: COLORS.logoZ }}>Z</Text>
-        <Text style={{ color: COLORS.logoA2 }}>A</Text>
-        <Text style={{ color: COLORS.logoA3 }}>A</Text>
-        <Text style={{ color: COLORS.logoR }}>R</Text>
-      </Text>
+      {showText && (
+        <Text style={[styles.text, { fontSize: textSize, marginLeft: spacing }]}>
+          <Text style={{ color: COLORS.logoB }}>B</Text>
+          <Text style={{ color: COLORS.logoA }}>A</Text>
+          <Text style={{ color: COLORS.logoZ }}>Z</Text>
+          <Text style={{ color: COLORS.logoA2 }}>A</Text>
+          <Text style={{ color: COLORS.logoA3 }}>A</Text>
+          <Text style={{ color: COLORS.logoR }}>R</Text>
+        </Text>
+      )}
     </View>
   )
 }
@@ -27,11 +35,9 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 22,
   },
   text: {
     fontWeight: '900',
-    marginLeft: 8,
     letterSpacing: 0.5,
   },
 })
